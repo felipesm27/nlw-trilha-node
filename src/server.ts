997a14e5c23195d8f9bffcc1a596f32d591cbcdf
Felipe Mello <felipesmello@hotmail.com>
2025-02-17 20:51:35 -0300

    feat: add schema validation with Zod

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,34 @@
 import fastify from "fastify";
 import { fastifyCors } from "@fastify/cors";
+import {
+  validatorCompiler,
+  serializerCompiler,
+} from "fastify-type-provider-zod";
+import { z } from "zod";
 
 const app = fastify();
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
 
 app.register(fastifyCors, {
   origin: "*",
 });
 
-app.get("/hello", () => {
-  return "hello";
-});
+app.post(
+  "/subscriptions",
+  {
+    schema: {
+      body: z.object({
+        name: z.string(),
+        email: z.string().email(),
+      }),
+    },
+  },
+  () => {
+    return "Hello World";
+  }
+);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server is running on port 3333");
