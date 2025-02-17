64dde0e4278d12259c940992f4f42bdf67bed915
Felipe Mello <felipesmello@hotmail.com>
2025-02-17 20:37:42 -0300

    feat: add CORS support

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import fastify from "fastify";
+import { fastifyCors } from "@fastify/cors";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.get("/hello", () => {
+  return "hello";
+});
 
 app.listen({ port: 3333 }).then(() => {
   console.log("Server is running on port 3333");
